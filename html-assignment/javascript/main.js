240dac9216aadceccffc8b3be28705f0e27dc5e0
best kindergartener <wale0027@algonquinlive.com>
2025-10-03 14:53:46 -0400

    tbh forgot i am doing an assignment and am adding javascript to handle deterministic styling, moves footer and header together, gets rid of clamp font size, changes font family

---
+++ html-assignment/javascript/main.js
@@ -0,0 +1,19 @@
+var headerOne = document.querySelector(".site-title");
+var headerText = headerOne.innerText;
+
+for ( var i = 0; i < headerText.length; i++) {
+    char = headerText.charAt(i);
+    if ( i === 0 ) {
+        // remove chars from list by replacing with 
+        // a div. div set to hidden for screenreaders
+        let emtpyDiv = document.createElement("div");
+        emtpyDiv.setAttribute("hidden", true);
+        headerOne.replaceChild(emtpyDiv, headerOne.firstChild);
+    }
+    var span = document.createElement("span");
+    span.innerHTML = char;
+    span.style = `--index:${i}`;
+    headerOne.appendChild(span, headerOne);
+    headerOne.style = `--total_letters:${headerOne.children.length}`
+    
+}
